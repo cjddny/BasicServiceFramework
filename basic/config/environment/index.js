893091507927d7aadec89122a0aeb63a206b6533
cjddny <cjddny@qq.com>
2016-05-13 10:09:47 +0800

    fix~

--- basic/config/environment/index.js
+++ basic/config/environment/index.js
@@ -7,5 +7,18 @@ var sequeConfig = require('../config.json');
 var rootPath = path.normalize(__dirname + '/../..');
 //All configurations
 var all = {
+
+  //server running environment
   env: process.env.NODE_ENV,
+
+  //workspace root path
+  root: rootPath,
+
+  //server port
+  port: process.env.PORT || 3008,
+
+  //server ip
+  ip: process.env.IP || '0.0.0.0'
 }
+
+module.exports = _.merge(all, require('./' + process.env.NODE_ENV + '.js') || {});
