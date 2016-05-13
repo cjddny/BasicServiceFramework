0809f48a1bded567e21ec223fa48024135b24bd2
cjddny <cjddny@qq.com>
2016-05-13 09:38:59 +0800

    initial

--- basic/config/environment/index.js
+++ basic/config/environment/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
 var path = require('path');
+var _ = require('lodash');
+var sequeConfig = require('../config.json');
+
+var rootPath = path.normalize(__dirname + '/../..');
+//All configurations
+var all = {
+  env: process.env.NODE_ENV,
+}
