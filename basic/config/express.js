853c24f1824bd6f91641c811af4a3efa350c471d
cjddny <cjddny@qq.com>
2016-05-30 16:02:49 +0800

    添加中间件处理~

--- basic/config/express.js
+++ basic/config/express.js
@@ -1,7 +1,32 @@
 'use strict';
 var express = require('express');
+var config = require('./environment');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 module.exports = function(app) {
-  //部署静态文件目录
+  //部署静态文件,视图文件目录
   app.use(express.static('public'));
+  app.set('views', config.root + '/server/views');
+  //配置视图引擎
+  app.engine('jade', require('jade').__express);
+  app.set('view engine', 'jade');
+  //配置通用中间件
+  app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-for']) {
+      req.clientIp = req.headers['x-forwarded-for'];
+    } else if (req.ip) {
+      req.clientIp = req.ip;
+    }
+    next();
+  });
+  //gzip静态资源压缩
+  app.use(compression());
+  //bodyParser json对象转化
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+  app.use(bodyParser.json());
+  app.use(cookieParser());
 }
